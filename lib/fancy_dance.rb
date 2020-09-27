346e2baeff04b4fc5a7fe64792a68c941348f297
Alex Darvin <amdarvin14@gmail.com>
2020-09-27 16:34:51 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -14,7 +14,7 @@ module FancyDance
     end
     
     def take_a_bow
-      "Thank you, thank you.It was a pleasure to dance fro you all."
+      "Thank you, thank you. It was a pleasure to dance fro you all."
     end
   end
   
